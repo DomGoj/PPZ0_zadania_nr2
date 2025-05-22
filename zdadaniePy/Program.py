7071862665e101acaae7cae7cc4d1830186e99be
DomGoj <dgoj9009@gmail.com>
2025-05-22 14:29:48 +0200

    Update Program.py

--- zdadaniePy/Program.py
+++ zdadaniePy/Program.py
@@ -41,6 +41,71 @@ class Konto:
         for t in self.transakcje:
             print(t)
         print(f"Saldo: {self.saldo} zl")
+def wybierz_konto(nazwa, konta):
+    return konta.get(nazwa)
+
+def main():
+    konta = {
+        "Test1": Konto("Test1", 1000),
+        "Test2": Konto("Test2", 2000)
+    }
+
+    while True:
+        print("\n--- MENU ---")
+        print("1. Wplata")
+        print("2. Wyplata")
+        print("3. Przelew")
+        print("4. Historia transakcji")
+        print("5. Wyjscie")
+        wybor = input("Wybierz opcje (1-5): ")
+
+        match wybor:
+            case "1":
+                nazwa = input("Konto (Test1/Test2): ")
+                konto = wybierz_konto(nazwa, konta)
+                if konto:
+                    kwota = float(input("Kwota: "))
+                    konto.wplata(kwota)
+                    print("Wplata wykonana")
+                else:
+                    print("Niepoprawna nazwa konta")
+
+            case "2":
+                nazwa = input("Konto (Test1/Test2): ")
+                konto = wybierz_konto(nazwa, konta)
+                if konto:
+                    kwota = float(input("Kwota: "))
+                    konto.wyplata(kwota)
+                    print("Wyplata wykonana")
+                else:
+                    print("Niepoprawna nazwa konta")
+
+            case "3":
+                nadawca = input("Nadawca (Test1/Test2): ")
+                odbiorca = input("Odbiorca (Test1/Test2): ")
+                konto_od = wybierz_konto(nadawca, konta)
+                konto_do = wybierz_konto(odbiorca, konta)
+                if konto_od and konto_do and konto_od != konto_do:
+                    kwota = float(input("Kwota: "))
+                    konto_od.przelew_do(konto_do, kwota)
+                    print("Przelew wykonany")
+                else:
+                    print("Blad: nieprawidlowe konta")
+
+            case "4":
+                nazwa = input("Konto (Test1/Test2): ")
+                konto = wybierz_konto(nazwa, konta)
+                if konto:
+                    konto.historia()
+                else:
+                    print("Niepoprawna nazwa konta")
+
+            case "5":
+                print("Wyjscie z programu")
+                break
+
+            case _:
+                print("Nieprawidlowy wybor")
 
 if __name__ == "__main__":
     main()
